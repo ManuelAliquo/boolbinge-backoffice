505cfa4b42afff92a17238a8de72d37cdc7d7716
ManuelAliquo <manuelaliquo2103@gmail.com>
2026-07-08 17:59:12 +0200

    add admin and protected dashboard

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,6 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -14,8 +13,9 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-            GenreTableSeeder::class,
-            ContentTableSeeder::class,
+            //GenreTableSeeder::class,
+            //ContentTableSeeder::class,
+            UserTableSeeder::class
         ]);
     }
 }
--- resources/views/profile/edit.blade_bk.php
+++ resources/views/profile/edit.blade_bk.php
@@ -1,9 +1,5 @@
 @extends('layouts.app')
 @section('content')
-<!-- Header -->
-<h2 class=" fw-semibold fs-2 text-secondary">
-    {{ __('Profile') }}
-</h2>
 
 <!-- Data -->
 <div class="py-12">
